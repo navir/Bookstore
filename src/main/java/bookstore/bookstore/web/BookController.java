57a0e02e3bc4e39576af0f550704d76d70c16808
Niilo Virtamo <niilo.virtamo@saunalahti.fi>
2024-02-25 23:18:52 +0200

    Tehtävä C3.3 valmis

--- src/main/java/bookstore/bookstore/web/BookController.java
+++ src/main/java/bookstore/bookstore/web/BookController.java
@@ -4,17 +4,19 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import bookstore.bookstore.model.Book;
 import bookstore.bookstore.model.BookRepository;
-
 
 @Controller
 public class BookController {
 
     @Autowired
-	private BookRepository repository; 
+    private BookRepository repository;
 
     @GetMapping("/booklist")
     public String listBooks(Model model) {
@@ -22,11 +24,30 @@ public class BookController {
         return "booklist";
     }
 
-    @RequestMapping(value = "/index", method=RequestMethod.GET)
+    @RequestMapping(value = "/addbook")
+    public String addStudent(Model model) {
+        model.addAttribute("book", new Book());
+        return "addbook";
+    }
+
+    @SuppressWarnings("null")
+    @PostMapping("/savebook")
+    public String save(Book book) {
+        repository.save(book);
+        return "redirect:booklist";
+    }
+
+    @SuppressWarnings("null")
+    @GetMapping("/delete/{id}")
+    public String deleteBook(@PathVariable("id") Long id, Model model) {
+        repository.deleteById(id);
+        return "redirect:../booklist";
+    }
+
+    @RequestMapping(value = "/index", method = RequestMethod.GET)
     public String showIndex(Model model) {
         System.out.println("Index endpoint");
         return "index";
     }
-    
 
 }
