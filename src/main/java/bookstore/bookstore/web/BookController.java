097ed35da4d78d59a8f4fef9b2da4e66bd196f76
Niilo Virtamo <niilo.virtamo@saunalahti.fi>
2024-02-25 22:45:13 +0200

    Tehtävä C3.2 valmis

--- src/main/java/bookstore/bookstore/web/BookController.java
+++ src/main/java/bookstore/bookstore/web/BookController.java
@@ -3,6 +3,7 @@ package bookstore.bookstore.web;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -14,6 +15,12 @@ public class BookController {
 
     @Autowired
 	private BookRepository repository; 
+
+    @GetMapping("/booklist")
+    public String listBooks(Model model) {
+        model.addAttribute("books", repository.findAll());
+        return "booklist";
+    }
 
     @RequestMapping(value = "/index", method=RequestMethod.GET)
     public String showIndex(Model model) {
